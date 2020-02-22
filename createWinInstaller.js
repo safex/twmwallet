cd0ee60c456769d9e180ac16f5e0176c2b4ea9b2
Daniel Dabek <xordaniel@gmail.com>
2020-02-21 21:13:52 -0500

    added additional files, include public, updated package.json

---
+++ createWinInstaller.js
@@ -0,0 +1,31 @@
+const createWindowsInstaller = require("electron-winstaller")
+  .createWindowsInstaller;
+const path = require("path");
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch(error => {
+    console.error(error.message || error);
+    process.exit(1);
+  });
+
+function getInstallerConfig() {
+  console.log("creating windows installer");
+  const rootPath = path.join("./");
+  const outPath = path.join(rootPath, "installers");
+
+  return Promise.resolve({
+    appDirectory: path.join(
+      rootPath,
+      "release-builds",
+      "TWMWallet-win32-ia32"
+    ),
+    authors: "Safex Developers",
+    noMsi: true,
+    outputDirectory: outPath,
+    exe: "SafexTWMWallet.exe",
+    setupExe: "SafexTWMWalletWindowsInstaller.exe",
+    setupIcon: "public/images/icons/wallet-icon2.ico",
+    skipUpdateIcon: true
+  });
+}
