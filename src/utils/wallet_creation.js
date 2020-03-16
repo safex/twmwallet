48c7477cbee53e678bbfe4905e893e46d3b7f8cd
dally <xordaniel@gmail.com>
2020-03-16 11:34:35 -0400

    add restoreHeight parameter when opening/restoring wallet

--- src/utils/wallet_creation.js
+++ src/utils/wallet_creation.js
@@ -1,37 +1,37 @@
-import path from "path";
-
 const safex = window.require("safex-nodejs-libwallet");
 
-export async function create_wallet(path, password, network, daemon) {
+export async function create_wallet(path, password, restore_height, network, daemon) {
     return safex.createWallet({
-        'path': path,
-        'password': password,
-        'network': network,
-        'daemonAddress': daemon,
+        path: path,
+        password: password,
+        network: network,
+        restoreHeight: restore_height,
+        daemonAddress: daemon,
     }).then((wallet) => {
         console.log(`created the wallet for address ${wallet.address()}`);
         return wallet;
     });
 }
 
-export async function open_wallet(path, password, network, daemon) {
+export async function open_wallet(path, password, restore_height, network, daemon) {
     return safex.openWallet({
-        'path': path,
-        'password': password,
-        'network': network,
-        'daemonAddress': daemon,
+        path: path,
+        password: password,
+        network: network,
+        restoreHeight: restore_height,
+        daemonAddress: daemon,
     }).then((wallet) => {
         console.log(`opened the wallet for address ${wallet.address()}`);
         return wallet;
     });
 }
-export async function recover_from_keys(path, password, network, daemon, address, viewkey, spendkey) {
+export async function recover_from_keys(path, password, restore_height, network, daemon, address, viewkey, spendkey) {
     return safex.createWalletFromKeys({
         path: path,
         password: password,
         network: network,
         daemonAddress: daemon,
-        restoreHeight: 0,
+        restoreHeight: restore_height,
         addressString: address,
         viewKeyString: viewkey,
         spendKeyString: spendkey
@@ -39,4 +39,9 @@ export async function recover_from_keys(path, password, network, daemon, address
         console.log(`recovered the wallet for address ${wallet.address()}`);
         return wallet;
     })
+}
+
+
+export function normalize_8decimals(balance) {
+    return Math.floor(parseFloat(balance) / 100000000) / 100;
 }
