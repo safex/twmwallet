c06da04b3dae1ce400a1727c8c140c6e1e720d72
dally <xordaniel@gmail.com>
2020-03-23 16:48:26 -0400

    update navigation

--- src/components/wallet/Navigation.js
+++ src/components/wallet/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {Link} from 'react-router-dom';
 
 export default class Navigation extends React.Component {
     constructor(props) {
@@ -8,6 +8,15 @@ export default class Navigation extends React.Component {
         this.state = {
         }
     }
+
+    componentDidMount() {
+
+        console.log("navigation loaded");
+        console.log(this.props.hello);
+        console.log(this.props.wallet);
+
+    }
+
 //add a reload function, reload the json_rpc and start over
     //probably need ipc here for electron process interaction
     render() {
@@ -18,11 +27,11 @@ export default class Navigation extends React.Component {
 
                     <div className="menu__right">
                         <ul className="menu__list">
-                            <li className="menu__list-item"><a className="menu__link" href="/wallet_home">Home</a></li>
+                            <li className="menu__list-item"><a className="menu__link" params={{wallet: this.props.wallet}} href="/wallet_home">Home</a></li>
                             <li className="menu__list-item"><a className="menu__link" href="#">Market</a></li>
                             <li className="menu__list-item"><a className="menu__link" href="#">Cash</a></li>
                             <li className="menu__list-item"><a className="menu__link" href="#">Tokens</a></li>
-                            <li className="menu__list-item"><a className="menu__link" href="/settings">Settings</a></li>
+                            <li className="menu__list-item"><Link className="menu__link" to="#" params={{wallet: this.props.wallet}} path="/settings">Settings</Link></li>
                             <li className="menu__list-item"><a className="menu__link" href="#">Bitcoin</a></li>
                         </ul>
                     </div>
