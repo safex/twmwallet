e7a43dc14f6faf20733f76fb39ee530b6361f5cc
dally <xordaniel@gmail.com>
2020-03-26 14:36:54 -0400

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,21 @@
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Route,
     Switch
 } from 'react-router-dom';
 import React from 'react';
 
 import CreateWallet from './components/entry/create_wallet';
+import OpenWallet from './components/entry/open_wallet';
+import RecoverKeys from "./components/entry/recover_keys";
 
 import SelectEntry from './components/entry/select_entry';
 import LegacyPassword from './components/entry/legacy/1password';
 import LegacyKeys from './components/entry/legacy/2loadkeys';
-//import ConvertLegacy from './components/entry/legacy/3makewallet';
+import ConvertLegacy from './components/entry/legacy/3makewallet';
 
 import WalletHome from './components/wallet/home';
+import Settings from './components/wallet/settings';
 
 
 
@@ -21,49 +24,17 @@ const routes = (
         <div>
             <Switch>
                 <Route exact path="/" component={SelectEntry}/>
-                <Route path="/legacy_password" component={LegacyPassword}/>
-                <Route path="/legacy_keys" component={LegacyKeys}/>
-                <Route path="/wallet_home" component={WalletHome}/>
-                <Route path="/create_wallet" component={CreateWallet}/>
-
+                <Route exact path="/recover_keys" component={RecoverKeys}/>
+                <Route exact path="/legacy_password" component={LegacyPassword}/>
+                <Route exact path="/legacy_keys" component={LegacyKeys}/>
+                <Route exact path="/from_legacy_wallet" component={ConvertLegacy}/>
+                <Route exact path="/create_wallet" component={CreateWallet}/>
+                <Route exact path="/open_wallet" component={OpenWallet}/>
+                <Route exact path="/wallet_home" component={WalletHome}/>
+                <Route exact name="settings" path="/settings" component={Settings}/>
             </Switch>
         </div>
     </Router>
 );
 
 export default routes;
-/*
-                <Route path="/from_legacy_wallet" component={ConvertLegacy}/>
-
-
-import ExistingWallet from './components/primary/entry/ExistingWallet';
-import CreateNew from './components/primary/entry/CreateNew';
-import RestoreWallet from './components/primary/entry/RestoreKeys';
-
-//legacy conversion paths
-import LegacyPassword from './components/primary/entry/legacy/1password';
-import LegacyKeys from './components/primary/entry/legacy/2loadkeys';
-import ConvertLegacy from './components/primary/entry/legacy/3makewallet';
-
-//wallet
-import WalletHome from './components/primary/wallet/home';
-import CashWallet from './components/primary/wallet/cash';
-const routes = (
-    <Router>
-        <div>
-            <Switch>
-                <Route exact path="/" component={Select_entry}/>
-                <Route path="/existing" component={ExistingWallet}/>
-                <Route path="/restore_keys" component={RestoreWallet}/>
-                <Route path="/create" component={CreateNew}/>
-                <Route path="/legacy_password" component={LegacyPassword}/>
-                <Route path="/legacy_keys" component={LegacyKeys}/>
-                <Route path="/convert_legacy" component={ConvertLegacy}/>
-                <Route path="/wallet_home" component={WalletHome}/>
-                <Route path="/cash_wallet" component={CashWallet}/>
-
-            </Switch>
-        </div>
-    </Router>
-);
- */
