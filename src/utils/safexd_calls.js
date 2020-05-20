c8aa8dd5255f9a3d35d2d73067e67dc5ce68cf12
dally <xordaniel@gmail.com>
2020-05-20 18:19:35 -0400

    add get staked tokens and rev share from blockchain node

--- src/utils/safexd_calls.js
+++ src/utils/safexd_calls.js
@@ -9,3 +9,23 @@ export async function get_chain_info(obj) {
         return resp.data;
     });
 }
+
+export async function get_staked_tokens(obj) {
+    return axios({
+        method: 'get',
+        url: 'http://' + obj.daemon_host + ':' + obj.daemon_port + '/get_staked_tokens',
+        data: obj,
+    }).then((resp) => {
+        return resp.data;
+    });
+}
+
+export async function get_interest_map(obj) {
+    return axios({
+        method: 'get',
+        url: 'http://' + obj.daemon_host + ':' + obj.daemon_port + '/get_interest_map',
+        data: obj,
+    }).then((resp) => {
+        return resp.data;
+    });
+}
