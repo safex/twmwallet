592c1abc9fd7d179a8cd67cbb8e9d37539db24e8
dally <xordaniel@gmail.com>
2020-09-24 18:01:42 -0400

    up version to 0.0.9

--- src/electron.js
+++ src/electron.js
@@ -61,7 +61,7 @@ function createWindow() {
                 label: "TWM Wallet",
                 submenu: [
                     {
-                        label: "About TWM Wallet v0.0.7",
+                        label: "About TWM Wallet v0.0.9",
                         selector: "orderFrontStandardAboutPanel:"
                     },
                     { type: "separator" },
