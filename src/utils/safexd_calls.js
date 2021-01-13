84a9aad58904e9f54006b8a700749a864b1586ed
dally <xordaniel@gmail.com>
2021-01-13 00:27:16 -0500

    push up utils to purchase contents retrieval

--- src/utils/safexd_calls.js
+++ src/utils/safexd_calls.js
@@ -40,3 +40,36 @@ export async function get_interest_map(obj) {
         return resp.data;
     });
 }
+
+export async function daemon_parse_transaction(data, out_put_type) {
+    let d_obj = {};
+    d_obj.jsonrpc = "2.0";
+    d_obj.id = 0;
+    d_obj.method = "decode_safex_output";
+    let h_obj = {};
+    h_obj.output_type = output_type;
+    h_obj.data = data;
+    d_obj.params = h_obj;
+    return axios({
+        method: 'post',
+        url: 'http://' + obj.daemon_host + ':' + obj.daemon_port + '/json_rpc',
+        data: d_obj
+    }).then((resp) => {
+        return resp.data.result;
+    })
+}
+
+async function get_transactions(txid) {
+    let tx_array = [];
+    tx_array.push(txid);
+    let t_obj = {};
+    t_obj.txs_hashes = tx_array;
+    t_obj.decode_as_json = true;
+    return axios({
+        method: 'post',
+        url: 'http://' + obj.daemon_host + ':' + obj.daemon_port +  '/get_transactions',
+        data: obj
+    }).then((resp) => {
+        return resp.data;
+    })
+}
