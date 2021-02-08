0465d7dedac5103e23cad06665255e04b361b9da
Milos Mrdakovic <milos@mrdak.dev>
2021-02-08 18:22:42 +0100

    Improve status and balance component

--- src/components/customComponents/HomeInfo.js
+++ src/components/customComponents/HomeInfo.js
@@ -1,47 +1,48 @@
-import React from 'react';
-
-import Loader from 'react-loader-spinner'
-
-// Icon Imports
-import { FaCubes } from 'react-icons/fa'
-
-import './ComponentCSS/HomeInfo.css'
+import React from "react";
+import { FaCubes } from "react-icons/fa";
+import "./ComponentCSS/HomeInfo.css";
+import sfxLogo from "../../img/sfx.svg";
+import sftLogo from "../../img/sft.svg";
 
 export default function HomeInfo(props) {
+  return (
+    <div className="home-info-box p-4 mt-4">
+      <h4 className="d-flex align-content-center">
+        <FaCubes className="mr-3" />
+        {props.walletHeight === props.blockHeight
+          ? props.blockHeight
+          : `${props.walletHeight} / ${props.blockHeight}`}
+      </h4>
 
-    return (
-       
-            <div className="home-info-box">
-                <h4>
-                    <FaCubes className="mr-3"/>
-                    { props.walletHeight === props.blockHeight ? 
-                        props.blockHeight 
-                    : 
-                        `${props.walletHeight} / ${props.blockHeight}`}
-                </h4>
+      <p>{props.connection}</p>
 
-                <p>{props.connection}</p>
+      <div className="d-flex flex-column">
+        <label>SAFEX CASH BALANCE</label>
+        <div className="d-flex align-items-center">
+          {props.firstRefresh === true
+            ? props.cashBalance.toLocaleString() + " SFX"
+            : "∞"}{" "}
+          <img className="ml-2" src={sfxLogo} width={20} alt="Safex Cash" />
+          <br />
+          {props.pendingCash > 0
+            ? `(${props.pendingCash.toLocaleString()} SFX Pending)`
+            : ""}
+        </div>
+      </div>
 
-                <h4 className={!props.firstRefresh ? 'infinity' : ''}>
-                    CASH: &nbsp;{ props.firstRefresh === true ?
-                                    (props.cashBalance.toLocaleString() + ' SFX')
-                                    :
-                                    ('∞')
-                    }
-                    <br/>
-                    {props.pendingCash > 0 ? `(${props.pendingCash.toLocaleString()} SFX Pending)` : ''}
-                    
-                    <br/>
-                    TOKENS: &nbsp;{ props.firstRefresh === true ?
-                                (props.tokenBalance.toLocaleString() + ' SFT')
-                                :
-                                ('∞')
-                    }
-                    <br/>
-                    {props.pendingTokens > 0 ? `(${props.pendingTokens.toLocaleString()} SFT Pending)` : ''}
-                </h4>
-                
-            </div>
-     
-    )
+      <div className="d-flex flex-column mt-4">
+        <label>SAFEX TOKEN BALANCE</label>
+        <div className="d-flex align-items-center">
+          {props.firstRefresh === true
+            ? props.tokenBalance.toLocaleString() + " SFT"
+            : "∞"}
+            <img className="ml-2" src={sftLogo} width={18} alt="Safex Cash" />
+          <br />
+          {props.pendingTokens > 0
+            ? `(${props.pendingTokens.toLocaleString()} SFT Pending)`
+            : ""}
+        </div>
+      </div>
+    </div>
+  );
 }
