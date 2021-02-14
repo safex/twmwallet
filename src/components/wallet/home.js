5ced5caf6716e09cb4d7662a0ef5cff37c14d4e8
Milos Mrdakovic <milos@mrdak.dev>
2021-02-14 13:54:06 +0100

    Make image modal responsive and centered

--- src/components/wallet/home.js
+++ src/components/wallet/home.js
@@ -3446,6 +3446,24 @@ class WalletHome extends React.Component {
                                             <ReactModal 
                                                 isOpen={!!this.state.show_modal_for_image}
                                                 onRequestClose={() => this.setState({show_modal_for_image: null})}
+                                                style={{
+                                                    overlay: {
+                                                        position: 'fixed',
+                                                        top: 0,
+                                                        left: 0,
+                                                        right: 0,
+                                                        bottom: 0,
+                                                        backgroundColor: 'rgba(255, 255, 255, 0.75)'
+                                                    },
+                                                    content: {
+                                                        top: '50%',
+                                                        left: '50%',
+                                                        right: 'auto',
+                                                        bottom: 'auto',
+                                                        marginRight: '-50%',
+                                                        transform: 'translate(-50%, -50%)'
+                                                    }
+                                                }}
                                                 >
                                     <CgClose
                                         className="pointer bg-white"
